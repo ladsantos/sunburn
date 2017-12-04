ca8e47bf0ac50e6926ef12c1cd81d68ee22d9b92
leonardo <leonardo.dossantos@unige.ch>
2017-12-04 17:02:55 +0100

    Implemented computation of errors and integrated flux

--- blastoise/analysis.py
+++ blastoise/analysis.py
@@ -36,15 +36,79 @@ class COSSpectrum(object):
             self.start_JD = self.meta[3].header['EXPSTRTJ']
             self.end_JD = self.meta[3].header['EXPENDJ']
 
+        # If ``good_pixel_limits`` is set to ``None``, then the data will be
+        # retrieved from the file in its entirety. Otherwise, it will be
+        # retrieved using the limits established by ``good_pixel_limits``
+        if self.gpl is None:
+            self.gpl = ((0, -1), (0, -1))
+        else:
+            pass
+
         # Extract the most important information from the data
-        self.wavelength = np.array([self.data['WAVELENGTH'][0],
-                                    self.data['WAVELENGTH'][1]])
-        self.flux = np.array([self.data['FLUX'][0], self.data['FLUX'][1]])
-        self.error = np.array([self.data['ERROR'][0], self.data['ERROR'][1]])
-        self.gross_counts = np.array([self.data['GCOUNTS'][0],
-                                      self.data['GCOUNTS'][1]])
-        self.background = np.array([self.data['BACKGROUND'][0],
-                                    self.data['BACKGROUND'][1]])
-        self.net = np.array([self.data['NET'][0], self.data['NET'][1]])
-        self.exp_time = np.array([self.data['EXPTIME'][0],
-                                  self.data['EXPTIME'][1]])
+        i00 = self.gpl[0][0]
+        i01 = self.gpl[0][1]
+        i10 = self.gpl[1][0]
+        i11 = self.gpl[1][1]
+        self.wavelength = np.array([self.data['WAVELENGTH'][0][i00:i01],
+                                    self.data['WAVELENGTH'][1][i10:i11]])
+        self.flux = np.array([self.data['FLUX'][0][i00:i01],
+                              self.data['FLUX'][1][i10:i11]])
+        self.error = np.array([self.data['ERROR'][0][i00:i01],
+                               self.data['ERROR'][1][i10:i11]])
+        self.gross_counts = np.array([self.data['GCOUNTS'][0][i00:i01],
+                                      self.data['GCOUNTS'][1][i10:i11]])
+        self.background = np.array([self.data['BACKGROUND'][0][i00:i01],
+                                    self.data['BACKGROUND'][1][i10:i11]])
+        self.net = np.array([self.data['NET'][0][i00:i01],
+                             self.data['NET'][1][i10:i11]])
+        self.exp_time = np.array([self.data['EXPTIME'][0][i00:i01],
+                                  self.data['EXPTIME'][1][i10:i11]])
+
+        # Instantiating useful global variables
+        self.sensitivity = None
+
+    # Compute the correct errors for the HST/COS observation
+    def compute_proper_error(self):
+        """
+
+        """
+        self.sensitivity = self.flux / self.net / self.exp_time
+        self.error = np.sqrt(self.gross_counts + 1.0) * self.sensitivity
+
+    # Compute the integrated flux in a given wavelength range
+    def integrated_flux(self, wavelength_range,
+                        uncertainty_method='quadratic_sum'):
+        """
+
+        Args:
+            wavelength_range:
+            uncertainty_method:
+
+        Returns:
+
+        """
+        min_wl = tools.nearest_index(self.wavelength, wavelength_range[0])
+        max_wl = tools.nearest_index(self.wavelength, wavelength_range[1])
+        int_flux = simps(self.flux[min_wl:max_wl],
+                         x=self.wavelength[min_wl:max_wl])
+
+        # Compute the uncertainty of the integrated flux
+        if uncertainty_method == 'quadratic_sum':
+            uncertainty = np.sqrt(np.sum(self.error[min_wl:max_wl] ** 2))
+        elif uncertainty_method == 'bootstrap':
+            n_samples = 10000
+            # Draw a sample of spectra and compute the fluxes for each
+            samples = np.random.normal(loc=self.flux[min_wl:max_wl],
+                                       scale=self.error[min_wl:max_wl],
+                                       size=[n_samples, max_wl - min_wl])
+            fluxes = []
+            for i in range(n_samples):
+                fluxes.append(simps(samples[i],
+                                    x=self.wavelength[min_wl:max_wl]))
+            fluxes = np.array(fluxes)
+            uncertainty = np.std(fluxes)
+        else:
+            raise ValueError('This value of ``uncertainty_method`` is not '
+                             'accepted.')
+
+        return int_flux, uncertainty
